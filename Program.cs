8368baa86478bcfbc95573ae3fba9a4e804de406
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-04-13 08:58:36 +0700

    member class

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarasi variabel 
+            int hasil;
+
+            //membuat objek dari class calculator 
+            Calculator calculator = new Calculator();
+
+            //panggil method Penjumlah
+            hasil = calculator.Penjumlah(10, 2);
+            CetakHasil("Hasil Penjumlah : " + hasil);
+
+            //panggil method penjumlah dengan 3 parameter (overload method)
+            hasil = calculator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil Penjumlah overload : " + hasil);
+
+            //panggil method pengurangan 
+            hasil = calculator.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan : " + hasil);
+
+            //panggil static method, langsung dari nama classnya 
+            hasil = Calculator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian : " + hasil);
+             
+            Console.ReadKey();
+        }
+        
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
